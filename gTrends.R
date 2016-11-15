f141b681329ab7d74155ce2f37458bea3f1f90e5
Patricio Vargas <pevargas@valdosta.edu>
2016-11-14 19:39:13 -0500

    install xml package

--- gTrends.R
+++ gTrends.R
@@ -1,12 +1,18 @@
 #Google Trends using gTrends Library
 #https://cran.r-project.org/web/packages/Rcpp/index.html
+#intall XML package
 install.packages("gtrendsR")
 install.packages("devtools")
 devtools::install_github("rstudio/shiny")
+require(ggplot2)
+require(gridExtra)
 #install XML package
 library(gtrendsR)
-usr <- "google email"  # alternatively store as options() or env.var
-psw <- "google password"        # idem
+usr <- "wearetamo@gmail.com"  # alternatively store as options() or env.var
+psw <- "Policia92"        # idem
 gconnect(usr, psw)       # stores handle in environment
-sport_trend <- gtrends(c("nhl", "nba", "nfl"))
-plot(sport_trend)        # data set also included in package
+company <- gtrends(c("Microsoft",geo = c("US")), start_date = "2014-01-01", end_date = "2016-11-01")
+plot(company)        # data set also included in package
+
+
+
